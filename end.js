ccaccd6ddc211ad3ce34ab183ea85d16de12f088
codeandcreateweb <codeandcreateweb@gmail.com>
2021-06-09 13:48:56 +1000

    init commit

---
+++ end.js
@@ -0,0 +1,33 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup',()=>{
+    console.log(username.value)
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore =(e)=>{
+    e.preventDefault();
+    const score ={
+        score: Math.floor(Math.random()*100),
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort((a, b)=>{
+        return b.score - a.score
+    })
+
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/")
+    console.log(highScores)
+};
